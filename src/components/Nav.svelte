0ddda1aeb0c7950d0d57cb3f288e85796b1798b5
valike23 <ludike23@gmail.com>
2022-02-21 10:19:04 +0100

    update this

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,19 +1,53 @@
 <div class="header">
-	<div class="navContainer">
-		<div class="contentWrapper navWrapper"><a rel="external" href="/"><img src="https://www.agrify.africa/assets/logo.svg" alt=""></a>
-			<div class="navWrapperInner"><a rel="external" href="/about">
-					<p>About</p>
-				</a><a rel="external" href="/agrify-affiliate">
-					<p>Agrify Affliate</p>
-				</a><a rel="external" href="/products">
-					<div class="buttonLarge undefined">Shop Agrify</div>
-				</a></div>
-		</div>
-	</div>
+  <div class="navContainer">
+    <div class="contentWrapper navWrapper">
+      <a href="/"><img src="../assets/logo.svg" alt="" class="siteLogo" /></a>
+      <div class="navWrapperInner">
+        <div class="searchContainer">
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            stroke-width="0"
+            viewBox="0 0 16 16"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+            ><path
+              fill-rule="evenodd"
+              d="M10.442 10.442a1 1 0 011.415 0l3.85 3.85a1 1 0 01-1.414 1.415l-3.85-3.85a1 1 0 010-1.415z"
+              clip-rule="evenodd"
+            /><path
+              fill-rule="evenodd"
+              d="M6.5 12a5.5 5.5 0 100-11 5.5 5.5 0 000 11zM13 6.5a6.5 6.5 0 11-13 0 6.5 6.5 0 0113 0z"
+              clip-rule="evenodd"
+            /></svg
+          ><input class="search" type="text" placeholder="Search Agrify.." />
+        </div>
+        <div class="tabs">
+          <a href="/about"><p>About</p></a><a href="/agrify-affiliate"
+            ><p>Agrify Affliate</p></a
+          ><a href="/products"><p>Shop Agrify</p></a>
+        </div>
+      </div>
+      <div class="buttons">
+        <a href="/login"><div class="signin">Sign In</div></a><a
+          href="/sign-up  "><div class="buttonSmall">Sign Up</div></a
+        ><svg
+          stroke="currentColor"
+          fill="currentColor"
+          stroke-width="0"
+          viewBox="0 0 1024 1024"
+          class="mobileMenuIcon"
+          height="1em"
+          width="1em"
+          xmlns="http://www.w3.org/2000/svg"
+          ><path
+            d="M904 160H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0 624H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0-312H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8z"
+          /></svg
+        >
+      </div>
+    </div>
+  </div>
 </div>
 
-<style>
-	.header {
-		font-family: 'Trap', sans-serif !important;
-	}
-</style>
+
